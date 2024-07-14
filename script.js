12a57de75c31b40494762b94ef1b100f71627295
LuuizPaes <lu.fernando.paes@hotmail.com>
2024-07-14 08:08:13 -0300

    inserindo funcoes no javascript, para o formulario.

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 let formulario = document.querySelector('.formulario')
 
+function aparecerBotao() {
+    formulario.style.left = "50%"
+}
+
+function desaparecerBotao() {
+    formulario.style.left = "-50%"
+}
